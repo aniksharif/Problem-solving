af843baeeb8f317cb48a0593369a360cf6dc5c80
Anik Sharif <aniksharif@gmail.com>
2019-08-06 16:02:10 +0600

    Trying

---
+++ Uri online judge/uri1984.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, c = 0, i, j = 0, k;
+    string d;
+
+    cin>>d;
+    for(i=d.length()-1;i>=0;i--)
+    {
+        cout<<d[i];
+    }
+    cout<<endl;
+
+    return 0;
+}
+
---
+++ Uri online judge/uri2160.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, c = 0, i, j = 0, k;
+    string d;
+
+    getline(cin,d);
+    if(d.length()<=80)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+    
+
+    return 0;
+}
+
